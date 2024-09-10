f24631ea654ebbf5c04959c44ef2280f9a72332e
Katherine Parshad <12345678+kathycodes247@users.noreply.github.com>
2024-09-10 20:07:06 +0530

    Your commit message

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favorite food</h2>
+            <p>We provide the best food in town</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
